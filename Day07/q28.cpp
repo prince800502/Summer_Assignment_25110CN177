4d2d8febc3a10924472ccf0584d9db3185da2217
Prince Singh Chauhan <ps800502@gmail.com>
2026-06-03 14:13:07 +0530

    Assignment done

--- Day07/q28.cpp
+++ Day07/q28.cpp
@@ -1,27 +1,19 @@
 #include<iostream>
-#include<math.h>
 using namespace std;
-int rev(int a);
+int rev(int a,int b);
 int main(){
     int n;
-    cout<<"Enter a positive number: ";
+    cout<<"Enter a number: ";
     cin>>n;
-    cout<<"Reverse of the given number is: "<<rev(n);
+    cout<<"Reverse of a number is: "<<rev(n,0);
     return 0;
 }
 
-int rev(int a){
-    if(a<10){
-        return a;
+int rev(int n,int r){
+    if(n==0){
+        return r;
     }
-    int count=0;
-    int x=a;
-    while(a>0){
-        a/=10;
-        count++;
-    }
-    int r=x%10;
-    int q=x/10;
-    int reverse = r*pow(10,count-1) + rev(q);
-    return reverse;
+    int last_digit = n%10;
+    r = r*10+  last_digit;
+    return rev(n/10, r);
 }
