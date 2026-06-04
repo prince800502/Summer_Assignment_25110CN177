658834cbdf0c21accec36b2a68501f64410ffb49
Prince Singh Chauhan <ps800502@gmail.com>
2026-06-04 12:59:16 +0530

    Assignment done

--- Day08/q31.cpp
+++ Day08/q31.cpp
@@ -4,11 +4,13 @@ int main(){
     int n;
     cout<<"Enter number of rows: ";
     cin>>n;
+    if(n<=26){
     for(int i=1;i<=n;i++){
         for(char j=65;j<=i+64;j++){
             cout<<j;
         }
         cout<<"\n";
     }
+}
     return 0;
 }
